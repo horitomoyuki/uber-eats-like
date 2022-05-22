22c45aca08c58629706944e720ef25bce3b31b08
horitomoyuki <eziltom0131@gmail.com>
2022-05-22 23:17:52 +0900

    Update Foods.jsx

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,20 +1,50 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
+
+// reducer
+import {
+  initialState as foodsInitialState,
+  foodsActionTyps,
+  foodsReducer,
+} from '../reducers/foods';
 
 // aips
 import { fetchFoods } from '../apis/foods';
 
+// constants
+import { REQUEST_STATE } from '../constants';
+
 export const Foods = (
   { match }
 ) => {
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
   useEffect(() => {
+    dispatch({ type: foodsActionTyps.FETCHING });
     fetchFoods(match.params.restaurantsId)
-    .then((data) =>
-      console.log(data)
-    )
+      .then((data) => {
+        dispatch({
+          type: foodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
+      })
   }, [])
   return (
     <Fragment>
-      フード一覧
+      {
+        foodsState.fetchState === REQUEST_STATE.LOADING ?
+          <Fragment>
+            <p>
+              ロード中...
+            </p>
+          </Fragment>
+        :
+          foodsState.foodsList.map(food =>
+            <div key={food.id}>
+              {food.name}
+            </div>
+          )
+      }
     </Fragment>
   )
 }
