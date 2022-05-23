50f5dae103c448dbf98c64ab2a883010ed2b5439
horitomoyuki <eziltom0131@gmail.com>
2022-05-24 08:32:03 +0900

    Update Foods.jsx

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -7,9 +7,7 @@ import { Link } from "react-router-dom";
 import { LocalMallIcon } from '../components/Icons';
 import { FoodWrapper } from '../components/FoodWrapper';
 import Skeleton from '@material-ui/lab/Skeleton';
-// --- ここから追加 ---
 import { FoodOrderDialog } from '../components/FoodOrderDialog';
-// --- ここまで追加 ---
 
 // reducers
 import {
@@ -57,6 +55,9 @@ const FoodsList = styled.div`
 const ItemWrapper = styled.div`
   margin: 16px;
 `;
+const submitOrder = () => {
+  console.log('登録ボタンが押された！')
+}
 
 export const Foods = ({
   match
@@ -128,14 +129,28 @@ export const Foods = ({
       </FoodsList>
       {
         state.isOpenOrderDialog &&
-          <FoodOrderDialog
-            food={state.selectedFood}
-            isOpen={state.isOpenOrderDialog}
-            onClose={() => setState({
-              ...state,
-              isOpenOrderDialog: false,
-            })}
-          />
+      <FoodOrderDialog
+        isOpen={state.isOpenOrderDialog}
+        food={state.selectedFood}
+        countNumber={state.selectedFoodCount}
+        onClickCountUp={() => setState({
+          ...state,
+          selectedFoodCount: state.selectedFoodCount + 1,
+        })}
+        onClickCountDown={() => setState({
+          ...state,
+          selectedFoodCount: state.selectedFoodCount - 1,
+        })}
+        // 先ほど作った関数を渡します
+        onClickOrder={() => submitOrder()}
+        // モーダルを閉じる時はすべてのstateを初期化する
+        onClose={() => setState({
+          ...state,
+          isOpenOrderDialog: false,
+          selectedFood: null,
+          selectedFoodCount: 1,
+        })}
+      />
       }
     </Fragment>
   )
